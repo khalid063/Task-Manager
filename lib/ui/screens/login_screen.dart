c82f34efc1e5fcf5d82573092523286e2f983ffd
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-22 10:15:36 +0600

    background widget make done

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../utils/assets_utils.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ScreenBackground(
+        child: Center(
+          child: Text('Login Screen'),
+        ),
+      ),
+    );
   }
 }
+
+
+class ScreenBackground extends StatelessWidget {
+  final Widget child;
+  const ScreenBackground({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: SvgPicture.asset(
+            AssetsUtils.backgroundSVG,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        child
+      ],
+    );
+  }
+}
+
