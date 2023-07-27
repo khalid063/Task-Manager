1370484e283b7a12c074f9814b3ea7a484333f21
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-27 19:14:18 +0600

    Email Verification is Dene Final

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/email_verification_screen.dart';
 import '../widgets/screen_background.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -9,65 +10,70 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    'Get Started With',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                SizedBox(height: 24,),
-                TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Email'
-                )
-              ),
-              SizedBox(height: 12,),
-              TextFormField(
-                obscureText: true,
-                  decoration: const InputDecoration(
-                      hintText: 'Password',
-                    /// if we want show and hide icon,
-                    //prefixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.remove_red_eye)),
-                  )
-              ),
-                SizedBox(height: 16,),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: () {}, child: Icon(Icons.arrow_forward_ios)),
-                ),
-                SizedBox(height: 16,),
-                Center(
-                child: InkWell(       ///*** for Removing the outside padding of Text Button
-                  onTap: () {},
-                  child: Text(
-                    'Forgot Password',
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ),
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(
+            child: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(24),
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
-                    "Don't have an account?",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.5,
+                    SizedBox(height: 64,),
+                    Text(
+                      'Get Started With',
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                  ),
-                  TextButton(onPressed: (){}, child: Text('Sign up')),
+                    SizedBox(height: 25,),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Email'
+                        )
+                    ),
+                    SizedBox(height: 12,),
+                    TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: 'Password',
+                          /// if we want show and hide icon,
+                          //prefixIcon: IconButton(onPressed: () {}, icon: Icon(Icons.remove_red_eye)),
+                        )
+                    ),
+                    SizedBox(height: 16,),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: Icon(Icons.arrow_forward_ios)),
+                    ),
+                    SizedBox(height: 16,),
+                    Center(
+                      child: InkWell(       ///*** for Removing the outside padding of Text Button
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => EmailVerificationScreen()));
+                        },
+                        child: Text(
+                          'Forgot Password',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Don't have an account?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                        TextButton(onPressed: (){}, child: Text('Sign up')),
+                      ],
+                    )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
-          ),
-        )
+          )
       ),
     );
   }
