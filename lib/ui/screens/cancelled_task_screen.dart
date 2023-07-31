c9262c48c1f649fe04745187c017cf3f98ecf9f2
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-31 11:29:07 +0600

    Cencel Screen Design is Done

--- lib/ui/screens/cancelled_task_screen.dart
+++ lib/ui/screens/cancelled_task_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/task_list_tile.dart';
+import '../widgets/user_profile_banner.dart';
+
 class CancelledTaskScreen extends StatelessWidget {
   const CancelledTaskScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const UserProfileBanner(),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (context, index){
+                  return TaskListTile();
+                }, separatorBuilder: (BuildContext context, int index) {
+                return Divider(height: 4,);
+              },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
