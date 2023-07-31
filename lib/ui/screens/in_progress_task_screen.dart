67ed4627b275132f2327ea1307c60e201c7a055d
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-31 11:14:30 +0600

    in progress screen Design is Done

--- lib/ui/screens/in_progress_task_screen.dart
+++ lib/ui/screens/in_progress_task_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/summary_card.dart';
+import '../widgets/task_list_tile.dart';
+import '../widgets/user_profile_banner.dart';
+
 class InProgressTaskScreen extends StatelessWidget {
   const InProgressTaskScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const UserProfileBanner(),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (context, index){
+                  return TaskListTile();
+                }, separatorBuilder: (BuildContext context, int index) {
+                return Divider(height: 4,);
+              },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
